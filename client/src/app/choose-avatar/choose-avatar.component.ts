4c265f070812cc6ea8f14749806cf36438b43876
anikeieva <alyonaoooooooo@gmail.com>
2018-10-16 13:43:09 +0300

    fixed saving selected default avatar after refreshing

--- client/src/app/choose-avatar/choose-avatar.component.ts
+++ client/src/app/choose-avatar/choose-avatar.component.ts
@@ -2,6 +2,8 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {SharedService} from "../shared/servises/shared.service";
 import {User} from "../shared/model/user";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {USER_STORAGE_TOKEN} from "../shared/model/userStorageToken";
+import {SESSION_STORAGE, StorageService} from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-choose-avatar',
@@ -15,10 +17,16 @@ export class ChooseAvatarComponent implements OnInit {
 
   constructor(private sharedService: SharedService,
               private dialogRef: MatDialogRef<ChooseAvatarComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any) {}
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              @Inject(SESSION_STORAGE) private storage: StorageService) {}
 
   ngOnInit() {
     this.sharedService.getUser().subscribe(user => this.user = user);
+
+    if (!this.user) {
+      this.user = this.storage.get(USER_STORAGE_TOKEN);
+    }
+    this.storage.set(USER_STORAGE_TOKEN, this.user);
 
     for (let n = 0; n <= this.maxAvatarsNumbers; n++) {
       this.avatars.push(`src/app/images/avatars/${this.user.gender}/${n}.png`);
@@ -30,6 +38,7 @@ export class ChooseAvatarComponent implements OnInit {
       this.user.avatar = avatar;
       this.sharedService.setUser(this.user);
       this.sharedService.updateUser.emit(this.user);
+      this.storage.set(USER_STORAGE_TOKEN, this.user);
     }
    }
 
