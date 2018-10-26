93f5f3e66562fec15d949b8c91e86de77ed35d67
anikeieva <alyonaoooooooo@gmail.com>
2018-10-26 14:36:53 +0300

    added client reaction at only log in user from server

--- client/src/app/user-log-in/user-log-in.component.ts
+++ client/src/app/user-log-in/user-log-in.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {UserLogInParam} from "../shared/model/userLogInParam";
 import {SocketService} from "../shared/servises/socket.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-log-in',
@@ -11,8 +12,11 @@ import {SocketService} from "../shared/servises/socket.service";
 export class UserLogInComponent implements OnInit {
   public userLogInFormParam: FormGroup;
   public userLogInParam: UserLogInParam;
+  public userIsAuthorized: boolean;
 
-  constructor(private  socketService: SocketService) {
+  constructor(private  socketService: SocketService,
+              private router: Router,
+              private route: ActivatedRoute) {
     this.userLogInFormParam = new FormGroup({
       login: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
@@ -20,6 +24,17 @@ export class UserLogInComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.socketService.onUserLogIn().subscribe((user) => {
+      this.userIsAuthorized = true;
+      this.router.navigate(['/chat'], { relativeTo: this.route });
+      console.log('user is authorized');
+    });
+
+    this.socketService.onUserNotLogIn().subscribe((userNotLogIn) => {
+      this.userIsAuthorized = false;
+      console.log('user is not authorized');
+    });
+
   }
 
   isEnabledLogIn() {
